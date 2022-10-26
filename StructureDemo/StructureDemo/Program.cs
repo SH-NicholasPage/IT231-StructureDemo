25bdef7fee3de6c6bc0b15b51dda842c457cd7e4
wheat32 <npage@southhills.edu>
2022-10-26 09:20:51 -0400

    Initial commit

---
+++ StructureDemo/StructureDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StructureDemo
+{
+    public class Program
+    {
+        private static String DateAsText { get; } = "10 23 2022";
+
+        public static void Main()
+        {
+            (int month, int day, int year) date = DateWithSubstring();
+            Console.WriteLine(date);
+            date = DateWithSpan();
+            Console.WriteLine(date);
+        }
+
+        public static (int month, int day, int year) DateWithSubstring()
+        {
+            String monthAsText = DateAsText.Substring(0, 2);
+            String dayAsText = DateAsText.Substring(3, 2);
+            String yearAsText = DateAsText.Substring(6);
+            int month = int.Parse(monthAsText);
+            int day = int.Parse(dayAsText);
+            int year = int.Parse(yearAsText);
+            return (month, day, year);
+        }
+
+        public static (int month, int day, int year) DateWithSpan()
+        {
+            ReadOnlySpan<Char> dateAsSpan = DateAsText;
+            ReadOnlySpan<Char> monthAsText = dateAsSpan.Slice(0, 2);
+            ReadOnlySpan<Char> dayAsText = dateAsSpan.Slice(3, 2);
+            ReadOnlySpan<Char> yearAsText = dateAsSpan.Slice(6);
+            int month = int.Parse(monthAsText);
+            int day = int.Parse(dayAsText);
+            int year = int.Parse(yearAsText);
+            return (month, day, year);
+        }
+    }
+}
---
+++ StructureDemo/StructureDemoBenchmark/Program.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace StructureDemoBenchmark
+{
+    public class Program
+    {
+        static void Main()
+        {
+            BenchmarkRunner.Run<MyBenchmark>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class MyBenchmark
+    {
+        private static String DateAsText { get; } = "10 26 2022";
+
+        [Benchmark]
+        //Tuple type return
+        public (int month, int day, int year) DateWithSubstring()
+        {
+            String monthAsText = DateAsText.Substring(0, 2);
+            String dayAsText = DateAsText.Substring(3, 2);
+            String yearAsText = DateAsText.Substring(6);
+            int month = int.Parse(monthAsText);
+            int day = int.Parse(dayAsText);
+            int year = int.Parse(yearAsText);
+            return (month, day, year);
+        }
+
+        [Benchmark]
+        public (int month, int day, int year) DateWithSpan()
+        {
+            ReadOnlySpan<char> dateAsSpan = DateAsText;
+            ReadOnlySpan<char> monthAsText = dateAsSpan.Slice(0, 2);
+            ReadOnlySpan<char> dayAsText = dateAsSpan.Slice(3, 2);
+            ReadOnlySpan<char> yearAsText = dateAsSpan.Slice(6);
+            int month = int.Parse(monthAsText);
+            int day = int.Parse(dayAsText);
+            int year = int.Parse(yearAsText);
+            return (month, day, year);
+        }
+    }
+}
